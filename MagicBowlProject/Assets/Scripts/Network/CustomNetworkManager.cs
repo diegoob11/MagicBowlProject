bab5503da108d1008054ffd996089113ac17b0c8
Daniel Lázaro <ls30414@salleurl.edu>
2018-03-16 19:42:35 +0100

    Network Manager Fixed
    Network Manager Fixed

--- MagicBowlProject/Assets/Scripts/Network/CustomNetworkManager.cs
+++ MagicBowlProject/Assets/Scripts/Network/CustomNetworkManager.cs
@@ -12,7 +12,6 @@ namespace UnityEngine.Networking
         public NetworkManager manager;
         private bool flag;
         private bool flag_disconnect;
-        private int i;
         private int flag2;
         public bool startGame = false;
 
@@ -20,7 +19,6 @@ namespace UnityEngine.Networking
         {
             flag = true;
             flag2 = 0;
-            i = 0;
             
             manager = this.GetComponent<NetworkManager>();
             manager.StartMatchMaker();
@@ -30,6 +28,7 @@ namespace UnityEngine.Networking
         {
             if (player == GetServerPlayer())
             {
+                Debug.Log("ASDASDASDASDASDSDDASDASD");
                 manager.matchMaker.DestroyMatch(manager.matches[0].networkId, 0, OnMatchDestroy);
             }
         }
@@ -48,30 +47,40 @@ namespace UnityEngine.Networking
         }
         void Update()
         {
-            //Si la lista de matches no esta vacia (null) && Si el count es superiror a 0
-            if (manager.matches != null && manager.matches.Count > 0 && flag == true && flag2 < 100)
+            //Debug.Log(manager.matches.Count);
+            //Si la lista de matches no es nula && Si el count de la lista de matches es mas grande que 0 && el flag2 es mas pequeño que 100, cosa
+            //que implica que lo acota a las primeras 100 interaciones, coge el primer match de la lista y la asigna al manager, se une.
+            
+            // Debug.Log(manager.matches);
+            // Debug.Log(manager.matches.Count);
+            
+            if (manager.matches != null && manager.matches.Count > 0 && flag == true && flag2 < 100 && manager.numPlayers >= 1)
             {
                 flag = false;
                 manager.matchName = manager.matches[0].name;
                 manager.matchSize = (uint)manager.matches[0].currentSize;
                 manager.matchMaker.JoinMatch(manager.matches[0].networkId, "", "", "", 0, 0, manager.OnMatchJoined);
             }
-            if (flag2 > 100 && flag == true)
+            //Si ha hecho mas de 100 iteraciones, y no ha encontrado partida (flag == true), crea la partida.
+            if (flag2 >= 100 && flag == true)
             {
+               
                 manager.matchName = "MagicBowl Game";
                 manager.matchSize = 4;
                 manager.matchMaker.CreateMatch(manager.matchName, manager.matchSize, true, "", "", "", 0, 0, manager.OnMatchCreate);
                 flag = false;
+
+                manager.matchMaker.ListMatches(0, 1, "", true, 0, 0, manager.OnMatchList);
+
+                //Debug.Log("After" + manager.matches[0]);
             }
-            // to change for 4
+            //Ha de ser igual a 4
             if(manager.numPlayers >= 1)
             {
                 startGame = true;
-            }
-            
+            }          
             flag2++;
-            i++;
-        
+
         }
     }
 };
