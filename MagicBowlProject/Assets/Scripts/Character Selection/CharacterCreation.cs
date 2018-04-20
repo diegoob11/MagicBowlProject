dc4a2f037b1d0e57cbe8d38c69a05d8846a63d7b
Diego Bellino <dbellinog@gmail.com>
2018-04-20 10:53:52 +0200

    character selection screen design done

--- MagicBowlProject/Assets/Scripts/Character Selection/CharacterCreation.cs
+++ MagicBowlProject/Assets/Scripts/Character Selection/CharacterCreation.cs
@@ -2,49 +2,104 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class CharacterCreation : MonoBehaviour {
+using UnityEngine.UI;
+public class CharacterCreation : MonoBehaviour
+{
 
-	public List<GameObject> models;
-	//Model index
-	public int selectionIndex;
-	void Start () {
-		selectionIndex = 0;
-		models = new List<GameObject>();
-		foreach(Transform t in transform) {
-			models.Add(t.gameObject);
-			t.gameObject.SetActive(false);
-		}
-		models[selectionIndex].SetActive(true);
-	}
-	public void Select(int index) {
-		if(index == selectionIndex) {
-			return;
-		}
-		if(index < 0 || index > models.Count) {
-			return;
-		}
-		models[selectionIndex].SetActive(false);
-		selectionIndex = index;
-		models[selectionIndex].SetActive(true);
-	}
+    public List<GameObject> models;
+    //Model index
+    public int selectionIndex;
 
-	public void findMatch() {
-		DontDestroyOnLoad(GameObject.Find("mc"));
-		
-		SceneManager.LoadScene("move_goal");
-		models[0].SetActive(false);
-		models[1].SetActive(false);
-	}
-	
-	public void BackToMain() {
-		SceneManager.LoadScene("MainMenu");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButton(0)) {
-			models[selectionIndex].transform.Rotate(new Vector3(0.0f, Input.GetAxis("Mouse X") * -3, 0.0f),
-				Space.Self);
-		}
-	}
+    public Image lockedMessage;
+    public List<Image> stats;
+    public List<Image> names;
+
+    void Start()
+    {
+        selectionIndex = 0;
+        models = new List<GameObject>();
+        foreach (Transform t in transform)
+        {
+            models.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+        models[selectionIndex].SetActive(true);
+    }
+    public void Select(int index)
+    {
+        if (index == selectionIndex)
+        {
+            return;
+        }
+        else if (index < 0 || index > models.Count)
+        {
+            StartCoroutine(ShowMessage(2));
+            return;
+        }
+        else
+        {
+            models[selectionIndex].SetActive(false);
+            selectionIndex = index;
+            models[selectionIndex].SetActive(true);
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                if (i == selectionIndex)
+                {
+                    names[i].enabled = true;
+                }
+                else
+                {
+                    names[i].enabled = false;
+                }
+            }
+
+            switch (selectionIndex)
+            {
+                case 0:
+                    stats[0].fillAmount = 0.5f;
+                    stats[1].fillAmount = 0.5f;
+                    stats[2].fillAmount = 0.5f;
+                    break;
+                case 1:
+                    stats[0].fillAmount = 0.75f;
+                    stats[1].fillAmount = 0.75f;
+                    stats[2].fillAmount = 0.25f;
+                    break;
+            }
+        }
+    }
+
+    IEnumerator ShowMessage(float delay)
+    {
+        lockedMessage.enabled = true;
+        yield return new WaitForSeconds(delay);
+        lockedMessage.enabled = false;
+    }
+
+    public void findMatch()
+    {
+        DontDestroyOnLoad(GameObject.Find("mc"));
+
+        SceneManager.LoadScene("move_goal");
+        foreach (GameObject model in models)
+        {
+            model.SetActive(false);
+        }
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetMouseButton(0))
+        // {
+        //     models[selectionIndex].transform.Rotate(new Vector3(0.0f, Input.GetAxis("Mouse X")
+        //         * -3, 0.0f), Space.Self);
+        // }
+    }
 }
