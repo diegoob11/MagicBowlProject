b37e1521fecf3aab677e684098e9f56cfd51efff
Daniel Lázaro <ls30414@salleurl.edu>
2018-05-16 17:09:45 +0200

    Loading Screen + Network Working
    That

---
+++ MagicBowlProject/Assets/Scripts/Game Rules/LoadingBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadingBall : MonoBehaviour {
+
+	private Vector3 posBall;
+	private float count;
+	// Use this for initialization
+	void Start () {
+		
+		count=0;
+		float x = gameObject.transform.position.x;
+		float y = gameObject.transform.position.y;
+		float z = gameObject.transform.position.z;
+		posBall = new Vector3(x,y,z);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count=count+0.05f;
+		//circular animation
+
+		posBall.x = posBall.x+((Mathf.Cos (count))/4);
+		posBall.z = posBall.z+((Mathf.Sin (count))/4);
+		gameObject.transform.position = posBall; //rotation around tower
+		gameObject.transform.Rotate(Vector3.up*4);//rotation around itself
+		
+	}
+}
---
+++ MagicBowlProject/Assets/Scripts/Game Rules/LoadingScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+
+public class LoadingScreen : MonoBehaviour {
+
+	public GameObject network;
+	private CustomNetworkManager net;
+	public GameObject mainCamera;
+	public GameObject loadingCamera;
+	public GameObject HUDCanvas;
+	public GameObject loadingCanvas;
+	
+	void Start(){
+		net = network.GetComponent<CustomNetworkManager> ();
+
+
+	}
+
+
+	void Update () {
+		
+		if (net.startGame) {
+			
+			//shows hudcanvas
+			loadingCanvas.SetActive(false);
+			CanvasGroup group = HUDCanvas.GetComponent<CanvasGroup> ();
+			group.alpha = 1;
+			group.interactable = true;
+
+			//changes camera
+			loadingCamera.SetActive(false);
+			mainCamera.SetActive (true);
+
+
+
+
+
+		}
+
+	}
+
+
+}
